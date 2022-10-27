686032925a34541378d8dbcdb11fe5f3bbdec8e1
Wonder Jung <wonder13662@gmail.com>
2022-10-27 09:41:08 +0900

    [#1]숫자 검사 추상화 및 메서드 주석 추가

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const REGEXP = {
+  // TODO 각 정규표현식에서 검사하는 조건을 주석으로 표현하는 것이 좋겠습니다. 
+  // 개발자 뿐만이 아닌 기획에서도 이 규칙을 확인할 필요가 있기 때문입니다.
+  // 그리고 개발자더라도 정규표현식이 길어지면 이해하기 조금 어려운 부분이 있네요.
   PASSWORD: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~])[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{10,}$/,
   PASSWORD_ALLOWED_TEXT: /^[A-Za-z\d!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]{0,}$/,
   EMAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
@@ -7,32 +10,110 @@ const REGEXP = {
   PHONE: /^\d{10,11}$/,
   CELL_PHONE: /^\d{10,11}$/,
 };
-
+// NOTE: validator 내부에서 자체 메서드를 참조하는 경우는 명확한 사유가 있어야 합니다.
+// 상호참조 및 의존성 관리를 위해서 내부 메서드 참조를 제한해야 합니다.
 export const validator = {
   // TODO null과 undefined는 분리되어야 하지 않을까?
-  // TODO 서비스에서 주로 사용하는 숫자 범위는 정수(integer)와 실수(float). 무한(Infinity)은 포함되지 않는다.
+  /**
+   * 유효한 number 타입인지 확인합니다.
+   * 정수(integer)와 실수(float), 무한(Infinity)은 유효합니다.
+   * 주의: 이 메서드는 다른 validator의 메서드를 참조하지 않는 atomic 메서드입니다.
+   *
+   * @param {number} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 number 타입 여부
+   */
   isNumber(v) {
     if (v === undefined ||
         v === null ||
         typeof v === 'string' ||
         typeof v === 'object' ||
         typeof v === 'symbol' ||
+        Array.isArray(v) || 
         Number.isNaN(v)) {
       return false;
     }
     return typeof v === 'number';
   },
+  /**
+   * 유효한 실수(float) 타입인지 확인합니다.
+   * 주의: 실수는 숫자의 부분집합이므로 isFloat 메서드를 참조합니다.
+   * NOTE: epsilon은 1과 1보다 큰 최소 실수값의 차를 의미
+   *
+   * @param {number} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 정수(Integer) 여부
+   */
+  isFloat(v) {
+    if (!this.isNumber(v) || 
+        v === Number.NEGATIVE_INFINITY ||
+        v === Number.POSITIVE_INFINITY || 
+        v !== Number.EPSILON) {
+      return false;
+    }
+    return true;
+  },
+  /**
+   * 유효한 정수(integer) 타입인지 확인합니다.
+   * 주의: 정수는 실수의 부분집합이므로 isFloat 메서드를 참조합니다.
+   *
+   * @param {number} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 정수(integer) 여부
+   */
   isInteger(v) {
-    if (!this.isNumber(v)) {
+    if (!this.isFloat(v)) {
       return false;
     }
     return parseInt(v, 10) === v;
   },
-  isTelephone(v) {
-    return !this.isNumber(v) && typeof v === 'string' && REGEXP.TEL.test(v);
+  /**
+   * 유효한 양의 정수(positive integer) 타입인지 확인합니다.
+   * 주의: 양의 정수는 정수의 부분집합이므로 isInteger 메서드를 참조합니다.
+   * 용도: Entity의 숫자형 id 검사
+   *
+   * @param {number} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 유효한 양의 정수(positive integer) 타입
+   */
+  isPositiveInteger(v) {
+    if (!this.isInteger(v)) {
+      return false;
+    }
+    return v > 0;
   },
+  /**
+   * 유효한 문자열(string) 타입인지 확인합니다.
+   * (기본)공백 문자열도 유효한 것으로 판단합니다.
+   * 주의: 이 메서드는 다른 validator의 메서드를 참조하지 않는 atomic 메서드입니다.
+   *
+   * @param {string} v - 검사할 값
+   * @param {number} minLength - 유효하다고 판단하는 문자열의 최소길이. 기본값은 0.
+   *
+   * @return {boolean} 검사할 값의 유효한 문자열(string) 여부
+   */
+  isString(v, minLength = 0) {
+    return typeof v === 'string' && v.length >= minLength;
+  },
+  /**
+   * 유효한 문자열(string):유선 전화번호(대한민국) 타입인지 확인합니다.
+   *
+   * @param {string} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 문자열(string):유선 전화번호(대한민국) 타입 여부
+   */
+  isTelephone(v) {
+    return this.isString(v) && REGEXP.TEL.test(v);
+  },
+  /**
+   * 유효한 문자열(string):휴대 전화번호(대한민국) 타입인지 확인합니다.
+   *
+   * @param {string} v - 검사할 값
+   *
+   * @return {boolean} 검사할 값의 문자열(string):휴대 전화번호(대한민국) 타입 여부
+   */
   isCellPhone(v) {
-    return !this.isNumber(v) && typeof v === 'string' && REGEXP.CELL_PHONE.test(v);
+    return this.isString(v) && REGEXP.CELL_PHONE.test(v);
   },
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ import assert from 'assert';
 // $ npm test
 
 describe('validator', () => {
-  it('isNumber', () => {
+  it('isNumber:valid', () => {
     // https://mochajs.org/#assertions
     // https://nodejs.org/api/assert.html
     assert.equal(validator.isNumber(1), true);
@@ -18,12 +18,14 @@ describe('validator', () => {
     assert.equal(validator.isNumber(-Infinity), true);
     assert.equal(validator.isNumber(Number.NEGATIVE_INFINITY), true);
     assert.equal(validator.isNumber(Number.POSITIVE_INFINITY), true);
-    assert.equal(validator.isNumber(Number.POSITIVE_INFINITY), true);
     assert.equal(validator.isNumber(Number.MIN_VALUE), true);
     assert.equal(validator.isNumber(Number.MAX_VALUE), true);
     assert.equal(validator.isNumber(Number.MAX_SAFE_INTEGER), true);
     assert.equal(validator.isNumber(Number.MIN_SAFE_INTEGER), true);
     assert.equal(validator.isNumber(Number.EPSILON), true);
+  });
+
+  it('isNumber:not valid', () => {
     assert.equal(validator.isNumber(NaN), false);
     assert.equal(validator.isNumber(null), false);
     assert.equal(validator.isNumber(undefined), false);
@@ -32,33 +34,58 @@ describe('validator', () => {
     assert.equal(validator.isNumber([]), false);
     assert.equal(validator.isNumber({}), false);
     assert.equal(validator.isNumber(Symbol()), false);    
-  });
+  });  
 
-  it('isInteger', () => {
+  it('isFloat:valid', () => {
+    assert.equal(validator.isFloat(1), true);
+    assert.equal(validator.isFloat(-1), true);
+    assert.equal(validator.isFloat(0), true);
+    assert.equal(validator.isFloat(Number.MAX_SAFE_INTEGER), true);
+    assert.equal(validator.isFloat(Number.MIN_SAFE_INTEGER), true);
+    assert.equal(validator.isFloat(0.1), true);
+    assert.equal(validator.isFloat(-1.1), true);
+    // NOTE: MIN_VALUE, MAX_VALUE는 실수임 (MDN 링크 추가 필요!)
+    assert.equal(validator.isFloat(Number.MIN_VALUE), true);
+    assert.equal(validator.isFloat(Number.MAX_VALUE), true);
+    // NOTE: elipson은 실수(float)임 (MDN 링크 추가 필요!)
+    assert.equal(validator.isFloat(Number.EPSILON), true);
+  });
+  
+  it('isFloat:not valid', () => {
+    // NOTE: isNumber 검사에서는 성공했으나 isFloat 에서는 실패
+    // NOTE: 무한(Infinity)은 실수가 아님 (MDN 링크 추가 필요!)
+    assert.equal(validator.isFloat(Infinity), false);
+    assert.equal(validator.isFloat(-Infinity), false);
+    assert.equal(validator.isFloat(Number.NEGATIVE_INFINITY), false);
+    assert.equal(validator.isFloat(Number.POSITIVE_INFINITY), false);
+  });   
+
+  it('isInteger:valid', () => {
     assert.equal(validator.isInteger(1), true);
     assert.equal(validator.isInteger(-1), true);
     assert.equal(validator.isInteger(0), true);
     assert.equal(validator.isInteger(Number.MAX_SAFE_INTEGER), true);
     assert.equal(validator.isInteger(Number.MIN_SAFE_INTEGER), true);
+  });
+
+  it('isInteger:not valid', () => {
+    // NOTE: isFloat 검사에서는 성공했으나 isInteger 에서는 실패
     assert.equal(validator.isInteger(0.1), false);
     assert.equal(validator.isInteger(-1.1), false);
-    assert.equal(validator.isInteger(Infinity), false);
-    assert.equal(validator.isInteger(-Infinity), false);
-    assert.equal(validator.isInteger(Number.NEGATIVE_INFINITY), false);
-    assert.equal(validator.isInteger(Number.POSITIVE_INFINITY), false);
-    assert.equal(validator.isInteger(Number.POSITIVE_INFINITY), false);
     assert.equal(validator.isInteger(Number.MIN_VALUE), false);
     assert.equal(validator.isInteger(Number.MAX_VALUE), false);
-    assert.equal(validator.isInteger(Number.EPSILON), false);
-    assert.equal(validator.isInteger(NaN), false);
-    assert.equal(validator.isInteger(null), false);
-    assert.equal(validator.isInteger(undefined), false);
-    assert.equal(validator.isInteger(''), false);
-    assert.equal(validator.isInteger('123'), false);
-    assert.equal(validator.isInteger([]), false);
-    assert.equal(validator.isInteger({}), false);
-    assert.equal(validator.isInteger(Symbol()), false);  
+  });  
+
+  it('isPositiveInteger:valid', () => {
+    assert.equal(validator.isPositiveInteger(1), true);
   });
+
+  it('isPositiveInteger:not valid', () => {
+    // NOTE: isInteger 검사에서는 성공했으나 isPositiveInteger 에서는 실패
+    assert.equal(validator.isPositiveInteger(1), true);
+    assert.equal(validator.isPositiveInteger(0), false);
+    assert.equal(validator.isPositiveInteger(-1), false);
+  });  
 
   it('isTelephone', () => {
     assert.equal(validator.isTelephone('12345678'), true);
