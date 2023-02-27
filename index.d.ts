fc3ae0ef33a81fa95486429036768a2f9e3d73e6
WonDongHyeon <wdh@barogo.com>
2023-02-27 15:33:43 +0900

    index.d.ts 수정

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,19 @@
 export type ValidationResult = string | boolean;
 export type ValidationFunction = (value: any) => ValidationResult;
 
+export declare const validator: {
+  isNumber: (value: any) => boolean;
+  isFloat: (value: any) => boolean;
+  isinteger: (value: any) => boolean;
+  isPositiveInteger: (value: any) => boolean;
+  isString: (value: any) => boolean;
+  isTelephone: (value: any) => boolean;
+  isCellPhone: (value: any) => boolean;
+  isEmail: (value: any) => boolean;
+  isUUID: (value: any) => boolean;
+  isBoolean: (value: any) => boolean;
+};
+
 interface IEssentials {
   lengthEqual: (length: number, msg?: ValidationResult) => ValidationFunction;
   lengthLess: (length: number, msg?: ValidationResult) => ValidationFunction;
@@ -13,6 +26,7 @@ interface IEssentials {
   betweenIncludeNumber: (min: number, max: number, msg?: ValidationResult) => ValidationFunction;
   isNumber: (msg?: ValidationResult) => ValidationFunction;
   isEmptyString: (msg?: ValidationResult) => ValidationFunction;
+  isEmptyArray(msg?: ValidationResult): ValidationFunction;
   isNull: (msg?: ValidationResult) => ValidationFunction;
   notNull: (msg?: ValidationResult) => ValidationFunction;
 }
@@ -25,7 +39,7 @@ interface IbRules extends IEssentials {
   passwordCharacter: (msg?: ValidationResult) => ValidationFunction;
   password: (msg?: ValidationResult) => ValidationFunction;
   tel: (msg?: ValidationResult) => ValidationFunction;
-  phone: (msg?: ValidationResult) => ValidationFunction;
+  cellPhone: (msg?: ValidationResult) => ValidationFunction;
 }
 
 export declare const bRules: IbRules;
