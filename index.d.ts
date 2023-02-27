fcbbdbd2e64be89e01e9a15c71d444026bc5ca70
WonDongHyeon <98367900+barogo-wdh@users.noreply.github.com>
2023-02-27 18:20:12 +0900

    Update index.d.ts (#13)

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,19 @@
 export type ValidationResult = string | boolean;
 export type ValidationFunction = (value: any) => ValidationResult;
 
+export declare const validator: {
+  isNumber: (value: any) => value is number;
+  isFloat: (value: any) => value is number;
+  isinteger: (value: any) => value is number;
+  isPositiveInteger: (value: any) => value is number;
+  isString: (value: any) => value is string;
+  isTelephone: (value: any) => value is string;
+  isCellPhone: (value: any) => value is string;
+  isEmail: (value: any) => value is string;
+  isUUID: (value: any) => value is string;
+  isBoolean: (value: any) => value is boolean;
+};
+
 interface IEssentials {
   lengthEqual: (length: number, msg?: ValidationResult) => ValidationFunction;
   lengthLess: (length: number, msg?: ValidationResult) => ValidationFunction;
@@ -13,6 +26,7 @@ interface IEssentials {
   betweenIncludeNumber: (min: number, max: number, msg?: ValidationResult) => ValidationFunction;
   isNumber: (msg?: ValidationResult) => ValidationFunction;
   isEmptyString: (msg?: ValidationResult) => ValidationFunction;
+  isEmptyArray(msg?: ValidationResult): ValidationFunction;
   isNull: (msg?: ValidationResult) => ValidationFunction;
   notNull: (msg?: ValidationResult) => ValidationFunction;
 }
@@ -25,7 +39,7 @@ interface IbRules extends IEssentials {
   passwordCharacter: (msg?: ValidationResult) => ValidationFunction;
   password: (msg?: ValidationResult) => ValidationFunction;
   tel: (msg?: ValidationResult) => ValidationFunction;
-  phone: (msg?: ValidationResult) => ValidationFunction;
+  cellPhone: (msg?: ValidationResult) => ValidationFunction;
 }
 
 export declare const bRules: IbRules;
